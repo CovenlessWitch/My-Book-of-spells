05e8a0a587c7aba028601caf9aae21fa3faed91b
CovenlessWitch <SaigeTheDarkMage@gmail.com>
2025-03-13 17:57:08 -0400

    2d Mechanics

---
+++ 2d_game_mechanics_1/Spawner.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{ 
+    public GameObject objectToSpawn;
+
+    public Transform spawnPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      SpawnObject();
+    }
+
+    void SpawnObject()
+    {
+        Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
